571944fe3fbe8edddb51b28513fa29d1be6a4c52
Jim Zhu <flappyboby@gmail.com>
2017-06-24 23:40:22 -0400

    Added smooth scrolling

--- src/js/ui.js
+++ src/js/ui.js
@@ -1,20 +1,45 @@
-(function (window, document) {
+$(document).ready(function() {
+    // Add smooth scrolling to all links
+    $("a").on('click', function(event) {
 
-    var layout   = document.getElementById('layout'),
-        menu     = document.getElementById('menu'),
+        // Make sure this.hash has a value before overriding default behavior
+        if (this.hash !== "") {
+            // Prevent default anchor click behavior
+            event.preventDefault();
+
+            // Store hash
+            var hash = this.hash;
+
+            // Using jQuery's animate() method to add smooth page scroll
+            // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 800, function() {
+
+                // Add hash (#) to URL when done scrolling (default click behavior)
+                window.location.hash = hash;
+            });
+        } // End if
+    });
+});
+
+(function(window, document) {
+
+    var layout = document.getElementById('layout'),
+        menu = document.getElementById('menu'),
         menuLink = document.getElementById('menuLink'),
-        content  = document.getElementById('main');
+        content = document.getElementById('main');
 
     function toggleClass(element, className) {
         var classes = element.className.split(/\s+/),
             length = classes.length,
             i = 0;
 
-        for(; i < length; i++) {
-          if (classes[i] === className) {
-            classes.splice(i, 1);
-            break;
-          }
+        for (; i < length; i++) {
+            if (classes[i] === className) {
+                classes.splice(i, 1);
+                break;
+            }
         }
         // The className is not found
         if (length === classes.length) {
@@ -33,7 +58,7 @@
         toggleClass(menuLink, active);
     }
 
-    menuLink.onclick = function (e) {
+    menuLink.onclick = function(e) {
         toggleAll(e);
     };
 
