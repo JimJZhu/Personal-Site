f2ba530c4c221f0373e4af97747dd1aa415391fe
JimJZhu <Jim.Jiaming.Zhu@gmail.com>
2017-12-20 00:25:24 -0500

    Added eslint

--- src/js/ui.js
+++ src/js/ui.js
@@ -1,71 +1,71 @@
-$(document).ready(function() {
-    // Add smooth scrolling to all links
-    $("a").on('click', function(event) {
+$(document).ready(() => {
+  // Add smooth scrolling to all links
+  $('a').on('click', (event) => {
+    // Make sure this.hash has a value before overriding default behavior
+    if (document.hash !== '') {
+      // Prevent default anchor click behavior
+      event.preventDefault();
 
-        // Make sure this.hash has a value before overriding default behavior
-        if (this.hash !== "") {
-            // Prevent default anchor click behavior
-            event.preventDefault();
+      // Store hash
+      const docHash = document.hash;
 
-            // Store hash
-            var hash = this.hash;
-
-            // Using jQuery's animate() method to add smooth page scroll
-            // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
-            $('html, body').animate({
-                scrollTop: $(hash).offset().top
-            }, 800, function() {
-
-                // Add hash (#) to URL when done scrolling (default click behavior)
-                window.location.hash = hash;
-            });
-        } // End if
-    });
+      // Using jQuery's animate() method to add smooth page scroll
+      // The optional number (800) specifies the number of millis it takes to scroll
+      $('html, body').animate(
+        {
+          scrollTop: $(docHash).offset().top,
+        },
+        800,
+        () => {
+          // Add hash (#) to URL when done scrolling (default click behavior)
+          window.location.hash = docHash;
+        },
+      );
+    } // End if
+  });
 });
 
-(function(window, document) {
+((window, document) => {
+  const layout = document.getElementById('layout');
+  const menu = document.getElementById('menu');
+  const menuLink = document.getElementById('menuLink');
+  const content = document.getElementById('main');
 
-    var layout = document.getElementById('layout'),
-        menu = document.getElementById('menu'),
-        menuLink = document.getElementById('menuLink'),
-        content = document.getElementById('main');
+  function toggleClass(element, className) {
+    const classes = element.className.split(/\s+/);
+    const len = classes.length;
+    let i = 0;
 
-    function toggleClass(element, className) {
-        var classes = element.className.split(/\s+/),
-            length = classes.length,
-            i = 0;
-
-        for (; i < length; i++) {
-            if (classes[i] === className) {
-                classes.splice(i, 1);
-                break;
-            }
-        }
-        // The className is not found
-        if (length === classes.length) {
-            classes.push(className);
-        }
-
-        element.className = classes.join(' ');
+    for (; i < len; i += 1) {
+      if (classes[i] === className) {
+        classes.splice(i, 1);
+        break;
+      }
+    }
+    // The className is not found
+    if (len === classes.length) {
+      classes.push(className);
     }
 
-    function toggleAll(e) {
-        var active = 'active';
+    element.className = classes.join(' ');
+  }
 
-        e.preventDefault();
-        toggleClass(layout, active);
-        toggleClass(menu, active);
-        toggleClass(menuLink, active);
+  function toggleAll(e) {
+    const active = 'active';
+
+    e.preventDefault();
+    toggleClass(layout, active);
+    toggleClass(menu, active);
+    toggleClass(menuLink, active);
+  }
+
+  menuLink.onclick = (e) => {
+    toggleAll(e);
+  };
+
+  content.onclick = (e) => {
+    if (menu.className.indexOf('active') !== -1) {
+      toggleAll(e);
     }
-
-    menuLink.onclick = function(e) {
-        toggleAll(e);
-    };
-
-    content.onclick = function(e) {
-        if (menu.className.indexOf('active') !== -1) {
-            toggleAll(e);
-        }
-    };
-
-}(this, this.document));
+  };
+})(window, window.document);
