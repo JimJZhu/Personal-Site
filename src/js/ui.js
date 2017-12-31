05d5a49107fcecd9a537e5ace193d5bda664edec
JimJZhu <Jim.Jiaming.Zhu@gmail.com>
2017-12-31 14:55:32 -0500

    Using Babel and es6

--- src/js/ui.js
+++ src/js/ui.js
@@ -2,13 +2,12 @@ $(document).ready(() => {
   // Add smooth scrolling to all links
   $('a').on('click', (event) => {
     // Make sure this.hash has a value before overriding default behavior
-    if (document.hash !== '') {
+    if (event.target.hash !== '') {
       // Prevent default anchor click behavior
       event.preventDefault();
 
       // Store hash
-      const docHash = document.hash;
-
+      const docHash = event.target.hash;
       // Using jQuery's animate() method to add smooth page scroll
       // The optional number (800) specifies the number of millis it takes to scroll
       $('html, body').animate(
