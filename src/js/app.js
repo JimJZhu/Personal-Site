07d958a71e977997e7575a28cc366733fc8651f9
JimJZhu <Jim.Jiaming.Zhu@gmail.com>
2018-01-24 19:55:13 -0500

    New look. Darker.

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,7 @@
-$(document).ready(() => {});
+$(document).on('scroll', () => {
+  if ($(document).scrollTop() >= 30) {
+    $('.header').addClass('shrink');
+  } else {
+    $('.header').removeClass('shrink');
+  }
+});
