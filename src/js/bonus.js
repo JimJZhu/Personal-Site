267721345f7a9c6bc2b938fb6931ce2f3617c139
JimJZhu <Jim.Jiaming.Zhu@gmail.com>
2018-03-04 06:03:59 -0500

    added quiz

--- src/js/bonus.js
+++ src/js/bonus.js
@@ -1,11 +1,12 @@
 const acc = document.getElementsByClassName('accordion');
+const request = require('browser-request');
+
 $(document).ready(() => {
   let i;
   for (i = 0; i < acc.length; i += 1) {
     acc[i].addEventListener('click', function () {
       /* Toggle between adding and removing the "active" class,
           to highlight the button that controls the panel */
-
       this.classList.toggle('active');
       /* Toggle between hiding and showing the active panel */
       const panel = this.children[0];
@@ -18,8 +19,171 @@ $(document).ready(() => {
       }
     });
   }
-});
+  request('../assets/pdf/c1.txt', (er, res) => {
+    // Parsing the text
 
+    if (!er) {
+      const body = res.body.split(/\.|\s|:/);
+      const QUESTION_STATE = 0;
+      const ANSWER_STATE_A = 1;
+      const ANSWER_STATE_B = 2;
+      const ANSWER_STATE_C = 3;
+      const ANSWER_STATE_D = 4;
+      const ANSWER_STATE_E = 5;
+      const CORRECT_ANSWER_STATE = 6;
+      const INITIAL_STATE = -1;
+      const questions = [];
+      let question = {};
+      let questionNum = 1;
+      let state = INITIAL_STATE;
+      console.log(body);
+      for (i = 0; i < body.length; i += 1) {
+        const token = body[i];
+        if (token.includes(`${questionNum})`)) {
+          if (state !== INITIAL_STATE) {
+            question.answer.e = question.answer.e.substr(0, question.answer.e.length - 8);
+            if (question.answer.a) {
+              questions.push(question);
+              console.log(question);
+            }
+          }
+          question = {
+            id: questionNum,
+            question: '',
+            answer: {
+              a: '',
+              b: '',
+              c: '',
+              d: '',
+              e: '',
+              correct: '',
+            },
+          };
+          questionNum += 1;
+          state = QUESTION_STATE;
+        } else if (token.includes('A)')) {
+          state = ANSWER_STATE_A;
+        } else if (token.includes('B)')) {
+          state = ANSWER_STATE_B;
+        } else if (token.includes('C)')) {
+          state = ANSWER_STATE_C;
+        } else if (token.includes('D)')) {
+          state = ANSWER_STATE_D;
+        } else if (token.includes('E)')) {
+          state = ANSWER_STATE_E;
+        } else if (token.substr(1) === 'Explanation') {
+          state = CORRECT_ANSWER_STATE;
+          console.log(token);
+        }
+        switch (state) {
+          case QUESTION_STATE:
+            question.question += `${token.match(/(\d{0,1}.*)/i)[1]} `;
+            if (question.question.includes('A)B)')) {
+              question.question = question.question.substr(10);
+            }
+            break;
+          case ANSWER_STATE_A:
+            question.answer.a += `${token} `;
+            break;
+          case ANSWER_STATE_B:
+            question.answer.b += `${token} `;
+            break;
+          case ANSWER_STATE_C:
+            question.answer.c += `${token} `;
+            break;
+          case ANSWER_STATE_D:
+            question.answer.d += `${token} `;
+            break;
+          case ANSWER_STATE_E:
+            question.answer.e += `${token} `;
+            break;
+          case CORRECT_ANSWER_STATE:
+            question.answer.correct = `${token.substr(0, 1)}`;
+            break;
+          default:
+            break;
+        }
+      }
+      // Appending to screen
+      const elem = $('#HRM');
+      for (i = 0; i < questions.length; i += 1) {
+        const q = questions[i];
+        const content = document.createElement('div');
+        const form = document.createElement('form');
+        form.setAttribute('class', 'pure-form');
+        const header = document.createElement('h3');
+        header.innerHTML = q.question;
+        form.appendChild(header);
+
+        const aLabel = document.createElement('label');
+        aLabel.setAttribute('for', 'radio');
+        aLabel.setAttribute('class', 'pure-checkbox');
+        const aCheckbox = document.createElement('input');
+        aCheckbox.id = 'a';
+        aCheckbox.type = 'radio';
+        aCheckbox.name = 'radio';
+        aCheckbox.value = 'a';
+        aLabel.appendChild(aCheckbox);
+        aLabel.innerHTML += q.answer.a;
+
+        const bLabel = document.createElement('label');
+        bLabel.setAttribute('for', 'radio');
+        bLabel.setAttribute('class', 'pure-checkbox');
+        const bCheckbox = document.createElement('input');
+        bCheckbox.id = 'b';
+        bCheckbox.type = 'radio';
+        bCheckbox.name = 'radio';
+        bCheckbox.value = 'b';
+        bLabel.appendChild(bCheckbox);
+        bLabel.innerHTML += q.answer.b;
+
+        const cLabel = document.createElement('label');
+        cLabel.setAttribute('for', 'radio');
+        cLabel.setAttribute('class', 'pure-checkbox');
+        const cCheckbox = document.createElement('input');
+        cCheckbox.id = 'c';
+        cCheckbox.type = 'radio';
+        cCheckbox.name = 'radio';
+        cCheckbox.value = 'c';
+        cLabel.appendChild(cCheckbox);
+        cLabel.innerHTML += q.answer.c;
+
+        const eLabel = document.createElement('label');
+        eLabel.setAttribute('for', 'radio');
+        eLabel.setAttribute('class', 'pure-checkbox');
+        const eCheckbox = document.createElement('input');
+        eCheckbox.id = 'e';
+        eCheckbox.type = 'radio';
+        eCheckbox.name = 'radio';
+        eCheckbox.value = 'e';
+        eLabel.appendChild(eCheckbox);
+        eLabel.innerHTML += q.answer.e;
+
+        const dLabel = document.createElement('label');
+        dLabel.setAttribute('for', 'radio');
+        dLabel.setAttribute('class', 'pure-checkbox');
+        const dCheckbox = document.createElement('input');
+        dCheckbox.id = 'd';
+        dCheckbox.type = 'radio';
+        dCheckbox.name = 'radio';
+        dCheckbox.value = 'd';
+        dLabel.appendChild(dCheckbox);
+        dLabel.innerHTML += q.answer.d;
+
+        form.appendChild(aLabel);
+        form.appendChild(bLabel);
+        form.appendChild(cLabel);
+        form.appendChild(dLabel);
+        form.appendChild(eLabel);
+        content.appendChild(form);
+        elem.append(content);
+      }
+    } else {
+      console.log('There was an error, but at least browser-request loaded and ran!');
+      throw er;
+    }
+  });
+});
 window.PIXI = require('phaser/build/custom/pixi');
 window.p2 = require('phaser/build/custom/p2');
 window.Phaser = require('phaser/build/custom/phaser-split');
